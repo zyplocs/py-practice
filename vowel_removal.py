ea1b36eaee92eaf4892290a684b85bd2e50aa534
zyplocs <zyplocked@gmail.com>
2025-08-01 23:24:35 -0500

    Complete first for loop practice script

---
+++ vowel_removal.py
@@ -0,0 +1,23 @@
+def consonant_prompt():
+    while True:
+        usr_in = input("Enter an alphabetical string: ")
+
+        if usr_in.isalpha():
+            vowels = ("a", "e", "i", "o", "u")
+            stringy_list = list(usr_in)
+            chars = []
+            
+            for z in stringy_list:
+                if z.lower() not in vowels:
+                    chars.append(z)
+            
+            vowelless = "".join(chars)
+                
+            print(f"Result: {vowelless}")
+            break
+        else:
+            print("Invalid input! Try again...")
+
+
+if __name__ == "__main__":
+    consonant_prompt()
