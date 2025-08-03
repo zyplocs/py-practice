f856f2d6faa851e0f10b211eafa18ad9a7b2a6c1
zyplocs <zyplocked@gmail.com>
2025-08-03 10:40:06 -0500

    Write `censor_prompt()` func

--- vowel_removal.py
+++ vowel_removal.py
@@ -3,11 +3,11 @@ def alpha_prompt():
         usr_in = input("Enter an alphabetical string: ")
 
         if usr_in.isalpha():
-            vowels = ("a", "e", "i", "o", "u")
-            stringy_list = list(usr_in)
+            vowels = {"a", "e", "i", "o", "u"}
+            stringy_in = list(usr_in)
             chars = []
             
-            for z in stringy_list:
+            for z in stringy_in:
                 if z.lower() not in vowels:
                     chars.append(z)
             
@@ -18,6 +18,27 @@ def alpha_prompt():
         else:
             print("Invalid input! Try again...")
 
+def censor_prompt():
+    while True:
+        usr_in = input("Enter a sentence: ")
+
+        if usr_in:
+            vowels = {"a", "e", "i", "o", "u"}
+            censored = []
+            
+            for i, val in enumerate(usr_in):
+                if val.lower() in vowels:
+                    print(f"Vowel found at index {i}")
+                    censored.append("*")
+                else:
+                    censored.append(val)
+
+            censored_in = "".join(censored)
+            print(f"Result: {censored_in}")
+            break
+        else:
+            print("That was nothing!")
+
 
 if __name__ == "__main__":
-    alpha_prompt()
+    censor_prompt()
