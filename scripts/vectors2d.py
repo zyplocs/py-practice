38b124c457e94b3a9f0b5e079b3e367102278e8f
zyplocs <zyplocked@gmail.com>
2025-08-20 01:29:07 -0500

    Remove whitespace from `vectors2d`

--- scripts/vectors2d.py
+++ scripts/vectors2d.py
@@ -13,31 +13,31 @@ def _to_float(input: SupportsFloat | str, *, name: str) -> float:
     try:
         if isinstance(input, bool):
             raise TypeError("Booleans are not accepted!")
-        
+
         floating = float(input)
     except (TypeError, ValueError) as e:
         raise NumericTypeError(f"{name.capitalize()} must be numeric; got {type(input).__name__}!") from e
-    
+
     if not isfinite(floating):
         raise ValueError(f"{name} must be finite; got {floating}!")
-    
+
     return floating
 
 def _coerce_vec(vec: Vector2DLike, name: str) -> Vector2D:
     x = _to_float(vec[0], name=f"{name} x")
     y = _to_float(vec[1], name=f"{name} y")
-    
+
     return (x, y)
 
 def vector_sum(vec1: Vector2DLike, vec2: Vector2DLike) -> Vector2D:
     v1 = _coerce_vec(vec1, "vec1")
     v2 = _coerce_vec(vec2, "vec2")
-    
+
     return (v1[0] + v2[0], v1[1] + v2[1])
 
 def vector_mag(vec: Vector2DLike, *, name: str="vec") -> float:
     x, y = _coerce_vec(vec, name)
-    
+
     return sqrt((x**2) + (y**2))
 
 def unit_vector(vec: Vector2DLike, *, name: str="vec") -> Vector2D:
@@ -55,7 +55,7 @@ def parse_vec(s: str, name: str) -> Vector2D:
         raise ValueError(f"{name} must be two comma-separated numbers!")
     x = _to_float(dims[0], name=f"{name} x")
     y = _to_float(dims[1], name=f"{name} y")
-    
+
     return (x, y)
 
 def main():
