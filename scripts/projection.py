1f2b9c2d309d330bc4a10baeab2f34171b7e11df
zyplocs <zyplocked@gmail.com>
2025-08-25 01:24:21 -0500

    Add `unit_vector()` and rename `l2_norm()`

--- scripts/projection.py
+++ scripts/projection.py
@@ -38,7 +38,16 @@ def dot(vec1: Vector2DLike, vec2: Vector2DLike) -> float:
 
     return sum(v1i*v2i for v1i, v2i in zip(v1, v2))
 
-def norm(vec: Vector2DLike, *, name: str="vec") -> float:
+def l2_norm(vec: Vector2DLike, *, name: str="vec") -> float:
     x, y = _coerce_vec(vec, name)
     
     return hypot(x, y)
+
+# 
+def unit_vector(vec: Vector2DLike, *, eps: float=1e-12, name: str="vec") -> Vector2D | None:
+    x, y = _coerce_vec(vec, name)
+
+    mag = hypot(x, y)
+    if mag <= eps:
+        return None
+    return (x/mag, y/mag)
