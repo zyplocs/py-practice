798d2b1c7d7bf2bdf5a7746d16917b1a21bcd73e
zyplocs <zyplocked@gmail.com>
2025-09-17 21:21:49 -0500

    Resurrect proper (legacy?) init style

--- scripts/linalg/__init__.py
+++ scripts/linalg/__init__.py
@@ -1,2 +1,11 @@
-# Initializer
+# linalg/__init__.py
 """Linear Algebra for Visual Applications"""
+
+from .vectors.vectors2d import Vector2D
+from .utils.guards import NumericTypeError
+
+__all__ = ['Vector2D', 'NumericTypeError']
+
+# Metadata
+__version__ = '0.1.0'
+__author__ = 'Eli M J'
